5a65397e2f1c76aef4af87b4d335466809d19303
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-06-11 13:26:38 -0500

    Semana 7

---
+++ app/src/main/java/edu/co/icesi/semana7kotlina/MainActivity.kt
@@ -0,0 +1,52 @@
+package edu.co.icesi.semana7kotlina
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var counterTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        counterTV = findViewById(R.id.counterTV)
+
+        runCounterThatIsGood()
+    }
+
+    var i: Int = 0
+
+    private fun runCounterThatFails() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            while (true) {
+                i++
+                delay(1000)
+                counterTV.text = "cuenta: $i"
+            }
+        }
+    }
+
+    private fun runCounterThatIsGood() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            while (true) {
+                i++
+                delay(1000)
+                lifecycleScope.launch(Dispatchers.Main) {
+                    counterTV.text = "cuenta: $i"
+                    Log.e(">>>","Alfa: $i")
+                }
+            }
+        }
+    }
+}
