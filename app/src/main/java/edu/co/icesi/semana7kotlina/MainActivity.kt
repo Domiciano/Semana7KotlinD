b0cbf20ac8387b357db47f622cc07e8d379b7852
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-06-11 19:52:30 -0500

    Semana 7

--- app/src/main/java/edu/co/icesi/semana7kotlina/MainActivity.kt
+++ app/src/main/java/edu/co/icesi/semana7kotlina/MainActivity.kt
@@ -9,21 +9,22 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.net.URL
+import java.net.URLConnection
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var counterTV: TextView
     private lateinit var counter: Counter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        counterTV = findViewById(R.id.counterTV)
 
         counter = ViewModelProvider(this).get(Counter::class.java)
         counter.counterLive.observe(this, Observer {
@@ -31,6 +32,43 @@ class MainActivity : AppCompatActivity() {
         })
 
         counter.start()
+
+
+        actionBtn.setOnClickListener {
+            val url = urlET.text.toString()
+            getRequestThatWorks(url)
+        }
+
+    }
+
+
+    //Aquí violamos la regla porque estamos usando Networking en el Main Thread
+    private fun getRequestThatFails(url: String) {
+        val url = URL(url)
+        val https = url.openConnection() as HttpsURLConnection
+        https.requestMethod = "GET"
+        var mensaje = ""
+        https.inputStream.bufferedReader().lines().forEach{
+            mensaje+=it
+        }
+        Log.e(">>>",mensaje)
+    }
+
+    //Aquí funciona porque se está invocando en un hilo worker
+    private fun getRequestThatWorks(url: String) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val url = URL(url)
+            val https = url.openConnection() as HttpsURLConnection
+            https.requestMethod = "GET"
+            var mensaje = ""
+            https.inputStream.bufferedReader().lines().forEach{
+                mensaje+=it
+            }
+            withContext(Dispatchers.Main){
+                outputTV.text = mensaje
+            }
+        }
+
     }
 
 }
